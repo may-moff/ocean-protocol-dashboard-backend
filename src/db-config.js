6f49a0b7b8f1b764e0e1bc250fe10a71f35505ba
Luca <luca.milanese90@gmail.com>
2021-06-15 14:27:16 +0200

    added local instance of mongodb

--- src/db-config.js
+++ src/db-config.js
@@ -1,18 +1,38 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connection = async () => {
-  try {
-    await mongoose.connect(process.env.DB_STR, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log('DB connected');
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const connection = async () => {
+//   try {
+//     await mongoose.connect(process.env.DB_STR, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     });
+//     console.log('DB connected');
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
+// module.exports = connection;
+
+const mongoose = require('mongoose');
+// const dbPath = "mongodb://<dbuser>:<dbpassword>@ds250607.mlab.com:38485/test-db";
+const dbPath = process.env.DB_URL;
+const connection = () => {
+  mongoose.connect(dbPath, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  const db = mongoose.connection;
+  db.on('error', () => {
+    console.log('> error occurred from the database');
+  });
+  db.once('open', () => {
+    console.log('> successfully opened the database');
+  });
+};
 module.exports = connection;
