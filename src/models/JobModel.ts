83143d7a5f24c938ec47254e822559f3fb2fc041
maya <mayelyimhoff@gmail.com>
2021-07-06 15:30:25 +0200

    added algorithm collection and correct references

--- src/models/JobModel.ts
+++ src/models/JobModel.ts
@@ -2,35 +2,33 @@ import { Schema, model, Mongoose, ObjectId } from "mongoose";
 
 export interface IJob {
   _id: string;
-  algo_id: number;
+  algorithm_id: ObjectId;
   data_id: number;
   user_id: ObjectId;
-  result: string;
-  //   result: array of objects
+  result: any;
 
   // get back on this to verify how to make it work better
   save(): any;
 }
 
 const jobSchema = new Schema<IJob>({
-  algo_id: {
-    type: Number,
-    default: () => Math.floor(Math.random() * 100),
-    required: true,
+  algorithm_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Algorithm",
   },
   data_id: {
     type: Number,
     default: () => Math.floor(Math.random() * 100),
     required: true,
   },
-  // correct way of referencing user_id?
+
   user_id: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
-  // does it work like this to get an array of strings??
+
   result: {
-    type: [String],
+    type: [Schema.Types.Mixed],
   },
 });
 
--- src/models/RuleModel.ts
+++ src/models/RuleModel.ts
@@ -2,11 +2,11 @@ import { Schema, model, Mongoose, ObjectId } from "mongoose";
 
 export interface IRule {
   _id: string;
-  algo_id: string;
-  //   ??
-  parse_keys: string;
-  //   ??
-  rules: string;
+  algorithm_id: ObjectId;
+
+  parse_keys: Array<string | boolean>;
+
+  rules: Array<string>;
   //   result: array of objects
 
   // get back on this to verify how to make it work better
@@ -14,21 +14,23 @@ export interface IRule {
 }
 
 const ruleSchema = new Schema<IRule>({
-  // algo_id: {
-  //     type: Schema.Types.ObjectId,
-  //     ref: "User",
-  //   },
-  //   can we get the algo_id from user collection like this?
-  algo_id: { type: String },
-  //   or should it be [Schema.Types.Mixed] ??
-  parse_keys: {
-    key: String,
-    data_type: String,
-    visualize: Boolean,
+  algorithm_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Algorithm",
   },
-  rules: {
-    type: [String],
-  },
+
+  parse_keys: [
+    {
+      key: String,
+      data_type: String,
+      visualize: Boolean,
+    },
+  ],
+  rules: [
+    {
+      type: String,
+    },
+  ],
 });
 
 export const RuleModel = model<IRule>("Rule", ruleSchema);
