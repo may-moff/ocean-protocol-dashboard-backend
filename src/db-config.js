d0990cc3e287630585c28ecb68e53998e0836727
ospi <lukasz.osipiak@gmail.com>
2021-07-06 10:41:04 +0200

    heroku commit

--- src/db-config.js
+++ src/db-config.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const dbPath = process.env.DB_URL;
+require("dotenv").config();
+const mongoose = require("mongoose");
+const dbPath = process.env.DB_STR;
 
 const connection = () => {
   mongoose.connect(dbPath, {
@@ -10,13 +10,13 @@ const connection = () => {
     useFindAndModify: false,
   });
   const db = mongoose.connection;
-  db.on('error', () => {
+  db.on("error", () => {
     console.log(
-      '> My dear Maya, it looks like there is a problem with the db, Luca will be available for you 24/7 to help you fix this'
+      "> My dear Maya, it looks like there is a problem with the db, Luca will be available for you 24/7 to help you fix this"
     );
   });
-  db.once('open', () => {
-    console.log('> My dear Maya, the db is ready for you');
+  db.once("open", () => {
+    console.log("> My dear Maya, the db is ready for you");
   });
 };
 module.exports = connection;
