1a4d61fc64fe8cd8e606f538dd52abb5382372e7
Luca <luca.milanese90@gmail.com>
2021-07-07 10:03:19 +0200

    apparent bug fix

--- src/routes/algo.ts
+++ src/routes/algo.ts
@@ -1,11 +1,11 @@
-const express = require("express");
-const controller = require("../controllers/algo");
+const express = require('express');
+const controller = require('../controllers/algo');
 // import * as controller from '../controllers/auth';
 
 export const algoRouter = express.Router();
 
 /** POST /api/algo */
 // algoRouter.post('/', controller.create);
-algoRouter.post("/", controller.create);
-algoRouter.get("/", controller.show);
-algoRouter.get("/", controller.index);
+algoRouter.post('/', controller.create);
+algoRouter.get('/', controller.show);
+algoRouter.get('/index', controller.index);
