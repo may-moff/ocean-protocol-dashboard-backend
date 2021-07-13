fe981445ccd561a97e6c183953617e643b2458ba
Luca <luca.milanese90@gmail.com>
2021-07-13 18:11:29 +0200

    modified new job res

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -1,22 +1,22 @@
-import { NextFunction, Request, Response } from "express";
-import { AlgorithmModel } from "../models/AlgorithmModel";
-import { JobModel } from "../models/JobModel";
-const fs = require("fs");
-const parseFunction = require("../parser.ts");
+import { NextFunction, Request, Response } from 'express';
+import { AlgorithmModel } from '../models/AlgorithmModel';
+import { JobModel } from '../models/JobModel';
+const fs = require('fs');
+const parseFunction = require('../parser.ts');
 
 module.exports.create = async (req: Request, res: Response) => {
   const userId: string = req.params.userId;
   const { algorithmId, dataId } = req.body;
 
   try {
-    if (!req.file) throw "file not available";
+    if (!req.file) throw 'file not available';
     const uploadLocation =
-      __dirname + "/../../public/demo/" + req.file.originalname;
+      __dirname + '/../../public/demo/' + req.file.originalname;
     fs.writeFileSync(
       uploadLocation,
       Buffer.from(new Uint8Array(req.file.buffer))
     );
-    const output = parseFunction(uploadLocation, ":", "#");
+    const output = parseFunction(uploadLocation, ':', '#');
     const job = new JobModel({
       algorithmId,
       dataId,
@@ -29,9 +29,18 @@ module.exports.create = async (req: Request, res: Response) => {
     const update = { parseKeys: output.parseKeys };
     await AlgorithmModel.findOneAndUpdate(filter, update);
 
-    res.status(200).json({ ...job, parseKeys: output.parseKeys });
+    res
+      .status(200)
+      .json({
+        result: output.result,
+        parseKeys: output.parseKeys,
+        algorithmId,
+        userId,
+        dataId,
+        filePath,
+      });
   } catch (error) {
-    if (error === "file not available")
+    if (error === 'file not available')
       res.status(400).send({ message: "Can't access file", error });
     res.status(400).send({ message: "Can't save job", error });
   }
