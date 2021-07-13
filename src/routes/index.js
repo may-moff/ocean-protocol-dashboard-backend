d870d298a0fed67c1baf854509f26a9eaab6f47a
Luca <luca.milanese90@gmail.com>
2021-07-13 14:55:34 +0200

    bugfixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,11 +15,11 @@ routes.use('/users', userRouter);
 routes.use('/algo', algoRouter);
 routes.use('/test/upload', upload.single('logBlob'), (req, res) => {
   let uploadLocation =
-    __dirname + '/../../public/demo/' + req.file.originalname; // where to save the file to. make sure the incoming name has a .webm extension
+    __dirname + '/../../public/demo/' + req.file.originalname;
   fs.writeFileSync(
     uploadLocation,
     Buffer.from(new Uint8Array(req.file.buffer))
-  ); // write the blob to the server as a file
+  );
   const output = parseFunction(uploadLocation, ':', '#');
   res.status(200).json(output);
 });
