3522a377b9e93c5d6f238ddee41e1ebff165f8ed
Luca <luca.milanese90@gmail.com>
2021-07-14 14:17:02 +0200

    fixed algo controller.update

--- src/controllers/algo.ts
+++ src/controllers/algo.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from "express";
-import { AlgorithmModel, IAlgorithm } from "../models/AlgorithmModel";
-import { JobModel } from "../models/JobModel";
-import { UserModel } from "../models/UserModel";
-const parseFunction = require("../parser.ts");
+import { NextFunction, Request, Response } from 'express';
+import { AlgorithmModel, IAlgorithm } from '../models/AlgorithmModel';
+import { JobModel } from '../models/JobModel';
+import { UserModel } from '../models/UserModel';
+const parseFunction = require('../parser.ts');
 
 // create
 // get
@@ -13,15 +13,15 @@ module.exports.create = async (req: Request, res: Response) => {
 
   try {
     const currentUser = await UserModel.findOne({ publicAddress: userId });
-    if (!currentUser) throw "user not found";
+    if (!currentUser) throw 'user not found';
     const findAlgo = await AlgorithmModel.findOne({ name });
     if (findAlgo) res.status(200).json(findAlgo);
     const algorithm = new AlgorithmModel({ name, userId: currentUser._id });
     await algorithm.save();
     res.status(200).json(algorithm);
   } catch (error) {
-    if (error === "user not found") {
-      res.status(400).send({ message: "Invalid user", error });
+    if (error === 'user not found') {
+      res.status(400).send({ message: 'Invalid user', error });
     }
 
     res.status(400).send({ message: "Can't save algorithm", error });
@@ -34,17 +34,17 @@ module.exports.show = async (req: Request, res: Response, data: any) => {
     const algorithm = await AlgorithmModel.findOne({ name });
     res.status(200).json(algorithm);
   } catch (error) {
-    res.status(400).send({ message: "cant get the algo", error });
+    res.status(400).send({ message: 'cant get the algo', error });
   }
 };
 
 module.exports.index = async (req: Request, res: Response) => {
   try {
-    console.log("hello");
+    console.log('hello');
     const algorithms = await AlgorithmModel.find({});
     res.status(200).json(algorithms);
   } catch (error) {
-    res.status(400).send({ message: "cant get all algos", error });
+    res.status(400).send({ message: 'cant get all algos', error });
   }
 };
 
@@ -54,13 +54,13 @@ module.exports.update = async (req: Request, res: Response) => {
 
   try {
     const filter = { _id: algorithmId };
-    const update = { parseKeys: req.body.parseKeys, rule: req.body.rule };
+    const update = { parseKeys: req.body.parseKeys, rules: req.body.rules };
     await AlgorithmModel.findOneAndUpdate(filter, update);
     const output = await parseFunction(
       req.body.filePath,
-      ":",
-      "#",
-      req.body.rule
+      ':',
+      '#',
+      req.body.rules
     );
     const jobFilter = { _id: req.body._id };
     const jobUpdate = { result: output.result };
@@ -70,6 +70,6 @@ module.exports.update = async (req: Request, res: Response) => {
       parseKeys: output.parseKeys,
     });
   } catch (error) {
-    res.status(400).send({ message: "no no no" });
+    res.status(400).send({ message: 'no no no' });
   }
 };
