d9cdf77685ddea406cc591454eff98777bb10a76
maya <mayelyimhoff@gmail.com>
2021-07-20 16:38:57 +0200

    users controller chech

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -11,13 +11,18 @@ module.exports.find = async (
     req.query && req.query.publicAddress
       ? { publicAddress: req.query.publicAddress }
       : undefined
-
   try {
+    if (!address) throw new Error('Invalid Address')
     const users = await UserModel.find(address)
-    res.json(users)
-  } catch (error) {
-    console.log(error)
+    if (!users[0]) throw new Error()
+    res.status(200).json(users[0])
     next()
+  } catch (error) {
+    if (error === 'Invalid Address') {
+      res.status(400).send({ message: 'Invalid Address' })
+    } else {
+      res.status(404).send({ message: 'User not found', error })
+    }
   }
 }
 
@@ -30,7 +35,7 @@ module.exports.create = async (
   try {
     const newUser = new UserModel({ publicAddress })
     await newUser.save()
-    res.json(newUser)
+    res.status(200).json(newUser)
   } catch (error) {
     console.log(error)
     next()
